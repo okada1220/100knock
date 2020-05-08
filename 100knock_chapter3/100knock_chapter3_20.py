d31d3d49de76f9bf4cf10496e1387860eb23276e
BC029 <1220.naoki7@gmail.com>
2020-05-08 15:42:47 +0900

    解決しました

--- 100knock_chapter3/100knock_chapter3_20.py
+++ 100knock_chapter3/100knock_chapter3_20.py
@@ -1,4 +1,8 @@
 import json
 
-with open('jawiki-country.json', 'r', encoding='utf-8') as file:
-    print(json.load(file))
+with open('jawiki-country.json', 'r', encoding='utf-8') as f:
+    for file in f:
+        json_file = json.loads(file)
+        if json_file['title'] == 'イギリス':
+            json_text = json_file['text']
+print(json_text)
