e624f1709db4b70b2fdc3aaf098ea5a98bcff991
BC029 <1220.naoki7@gmail.com>
2020-05-08 19:36:56 +0900

    改善しました
    listとstrの互換を実装しました

--- 100knock_chapter1/100knock_chapter1_09.py
+++ 100knock_chapter1/100knock_chapter1_09.py
@@ -8,11 +8,9 @@ words = text.split()
 changed_text = ''
 for word in words:
     if len(word) > 4:
-        middle_word = [w for w in word[1:len(word)-1]]
+        middle_word = list(word[1:len(word)-1])
         random.shuffle(middle_word)
-        middle_words = ''
-        for i in range(len(middle_word)):
-            middle_words += middle_word[i]
+        middle_words = ''.join(middle_word)
         changed_text += word[0] + middle_words + word[len(word)-1] + ' '
     else:
         changed_text += word + ' '
