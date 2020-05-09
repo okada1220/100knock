f95ed862ac456259dd50933fbdf3c821d37c87d8
BC029 <1220.naoki7@gmail.com>
2020-05-09 11:27:34 +0900

    改善しました
    collectionsのCounterクラスを用いて実装しました

--- 100knock_chapter2/100knock_chapter2_19.py
+++ 100knock_chapter2/100knock_chapter2_19.py
@@ -1,31 +1,13 @@
-f = open('popular-names.txt', 'r', encoding='utf-8')
-file = f.read()
-f.close()
+import collections
 
-texts = file.split('\n')
-texts.remove('')
+text_name = []
+with open('popular-names.txt', 'r', encoding='utf-8') as file:
+    for texts in file:
+        text = texts.split()
+        text_name.append(text[0])
 
-for i in range(len(texts)):
-    texts[i] = texts[i].split('\t')
-
-frequency_word = [[0 for i in range(2)] for j in range(len(texts))]
-number = 0
-for x in range(len(texts)):
-    frag = 0
-    for y in range(number):
-        if texts[x][0] == frequency_word[y][0]:
-            frequency_word[y][1] += 1
-            frag = 1
-    if frag == 0:
-        frequency_word[number][0] = texts[x][0]
-        frequency_word[number][1] = 1
-        number += 1
+frequency_word = collections.Counter(text_name)
 print(frequency_word)
 
-for n in range(len(frequency_word)-1, 0, -1):
-    for m in range(0, n):
-        if frequency_word[m][1] < frequency_word[m+1][1]:
-            sub = frequency_word[m]
-            frequency_word[m] = frequency_word[m+1]
-            frequency_word[m+1] = sub
+frequency_word.most_common()
 print(frequency_word)
