7bb1ac303df3960ff5d12321c4db1e13d370cd75
BC029 <1220.naoki7@gmail.com>
2020-05-09 10:00:34 +0900

    改善しました
    sort()でソートを行いました

--- 100knock_chapter2/100knock_chapter2_18.py
+++ 100knock_chapter2/100knock_chapter2_18.py
@@ -9,11 +9,6 @@ for i in range(len(texts)):
     texts[i] = texts[i].split('\t')
 print(texts)
 
-for x in range(len(texts)-1, 0, -1):
-    for y in range(0, x):
-        if int(texts[y][2]) < int(texts[y+1][2]):
-            sub = texts[y]
-            texts[y] = texts[y+1]
-            texts[y+1] = sub
+texts.sort(key=lambda x: int(x[2]), reverse=True)
 print('ソート後')
 print(texts)
