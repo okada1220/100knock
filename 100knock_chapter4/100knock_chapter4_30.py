61608d58991fb563a9512214fe411824301cfaf9
BC029 <1220.naoki7@gmail.com>
2020-05-14 16:07:35 +0900

    進捗
    ７行目：lineがEOS\nの時、参照エラーが発生するのでif文で除去しました。
    ８行目：line.split()だと２語以上の文字での分割が出来なかったのでre.split()を用いました。
    ９行目：listがポインタであるからlistをfor文の外で定義するとmap.append()されるlistが全て更新されるので、逐一新しいlistであるようfor文の中に入れました。

---
+++ 100knock_chapter4/100knock_chapter4_30.py
@@ -0,0 +1,16 @@
+import re
+import pprint
+
+map = []
+with open('neko.txt.mecab', 'r', encoding='utf-8') as f:
+    for line in f:
+        if not line == 'EOS\n':
+            info = re.split(r'\t|,', line)
+            list = {}
+            list['surface'] = info[0]
+            list['base'] = info[7]
+            list['pos'] = info[1]
+            list['pos1'] = info[2]
+            map.append(list)
+
+pprint.pprint(map)
