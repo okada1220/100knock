b8625a7e7d33ef318557e8b42dfffcb8f8baa7d4
BC029 <1220.naoki7@gmail.com>
2020-05-14 14:03:22 +0900

    変更しました
    re.compileをループ一つ外に出してみました。
    ただ、そのせいで出力がWebページのトップから出現順だったのが、レベル順になってしまいました。

--- 100knock_chapter3/100knock_chapter3_23.py
+++ 100knock_chapter3/100knock_chapter3_23.py
@@ -9,9 +9,9 @@ with open('jawiki-country.json', 'r', encoding='utf-8') as f:
 
 json_line = json_text.split()
 
-for i in range(len(json_line)):
-    for n in range(2, 5):
-        session_compile = re.compile("^={" + str(n) + "}(\s?\w+\s?)={" + str(n) + "}")
+for n in range(2, 5):
+    session_compile = re.compile("^={" + str(n) + "}(\s?\w+\s?)={" + str(n) + "}")
+    for i in range(len(json_line)):
         session_search = session_compile.search(json_line[i])
         if bool(session_search):
             print(session_search.group(), n-1)
