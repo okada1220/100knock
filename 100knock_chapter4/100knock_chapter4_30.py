763154d47240df4117660d4df33afd656fbf6667
BC029 <1220.naoki7@gmail.com>
2020-05-15 11:37:45 +0900

    改善しました
    関数make_morphを定義し、main()で処理が行われるようにしました。

--- 100knock_chapter4/100knock_chapter4_30.py
+++ 100knock_chapter4/100knock_chapter4_30.py
@@ -1,16 +1,23 @@
 import re
 import pprint
 
-morph = []
-with open('neko.txt.mecab', 'r', encoding='utf-8') as f:
-    for line in f:
-        if not line == 'EOS\n':
-            info = re.split(r'\t|,', line)
-            key_list = {}
-            key_list['surface'] = info[0]
-            key_list['base'] = info[7]
-            key_list['pos'] = info[1]
-            key_list['pos1'] = info[2]
-            morph.append(key_list)
+def make_morph(filename):
+    morph = []
+    with open(filename, 'r', encoding='utf-8') as f:
+        for line in f:
+            if not line == 'EOS\n':
+                info = re.split(r'\t|,', line)
+                key_list = {}
+                key_list['surface'] = info[0]
+                key_list['base'] = info[7]
+                key_list['pos'] = info[1]
+                key_list['pos1'] = info[2]
+                morph.append(key_list)
+    return morph
 
-pprint.pprint(morph)
+def main():
+    morph = make_morph('neko.txt.mecab')
+    pprint.pprint(morph)
+
+if __name__ == '__main__':
+    main()
