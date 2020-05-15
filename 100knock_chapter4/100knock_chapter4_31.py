52887d71b21401067ecc43d308f9cb3f3b5e681f
BC029 <1220.naoki7@gmail.com>
2020-05-15 12:17:01 +0900

    改善しました
    他のファイルの関数を使えるようimportしました。
    通常通り、import文で行うとファイル名に数字が含まれる場合、エラーが発生しimportできなかった。
    __import__を使うことでそれを回避することが出来た。
    これからはファイル名に数字、ハイフン、ピリオドなどの文字を加えないように気を付けたい。

--- 100knock_chapter4/100knock_chapter4_31.py
+++ 100knock_chapter4/100knock_chapter4_31.py
@@ -1,18 +1,13 @@
 import re
 import pprint
+mgs = __import__('100knock_chapter4_30')
 
-morph = []
-with open('neko.txt.mecab', 'r', encoding='utf-8') as f:
-    for line in f:
-        if not line == 'EOS\n':
-            info = re.split(r'\t|,', line)
-            key_list = {}
-            key_list['surface'] = info[0]
-            key_list['base'] = info[7]
-            key_list['pos'] = info[1]
-            key_list['pos1'] = info[2]
-            morph.append(key_list)
+def main():
+    morph = mgs.make_morph('neko.txt.mecab')
 
-for i in range(len(morph)):
-    if morph[i]['pos'] == '動詞':
-        print(morph[i]['surface'])
+    for i in range(len(morph)):
+        if morph[i]['pos'] == '動詞':
+            print(morph[i]['surface'])
+
+if __name__ == '__main__':
+    main()
