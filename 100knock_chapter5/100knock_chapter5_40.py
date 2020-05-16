d8a9628b2273f6177f6abd49c9e7b8bf6287c3d6
BC029 <1220.naoki7@gmail.com>
2020-05-17 05:52:11 +0900

    進捗
    5~10行目：class Morphを定義しました。
    13~15行目：MeCab用のmecabを定義しました。行ごとのMorphオブジェクトのリストをリストでまとめてAll_Morph_listとしました。途中で使うMorph_listを最初だけ初期化が行えるようにfragを定義しました。
    18~29行目：neko.txt.cabochaを各行ごと読み込んで、余分なものを除いて言葉だけの所にしてmecab.parase(line).split('\n')で単語分割を行いました。さらに余分な部分を除き、chapter4_30と同様に抽出を行いました。
    30~33行目：Morph_listに各行のMorphオブジェクトのリストが入っているので、あればAll_Morph_listに加えて、次の行のためにMorphを初期化しいます。
    35~38行目：３文目の形態素列を表示しています。
    
    追記：
    CaboChaをWindows上のPythonで使えるようにするだけで死にそうになりました。CaboChaが32bitにしかないので、コンパイルに使っているAnaconda内のPythonを64bitから32bitに変更しないといけないことに気が付くまで苦戦しました。

---
+++ 100knock_chapter5/100knock_chapter5_40.py
@@ -0,0 +1,41 @@
+import MeCab
+import re
+import pprint
+
+class Morph:
+    def __init__(self, morph_list):
+        self.surface = morph_list[0]
+        self.base = morph_list[7]
+        self.pos = morph_list[1]
+        self.pos1 = morph_list[2]
+
+def main():
+    mecab = MeCab.Tagger()
+    All_Morph_list = []
+    frag = 0
+    with open('neko.txt.cabocha', 'r', encoding='utf-8') as file:
+        for line in file:
+            if not line == 'EOS\n':
+                if frag == 0:
+                    Morph_list = []
+                    frag = 1
+                line.strip()
+                line = re.sub(r'[-+D]|\s', '', line)
+                word = mecab.parse(line).split('\n')
+                word.remove('')
+                word.remove('EOS')
+                for morph_info in word:
+                    morph_list = re.split(r'\t|,', morph_info)
+                    Morph_list += [Morph(morph_list)]
+            else:
+                if len(Morph_list) > 0:
+                    All_Morph_list.append(Morph_list)
+                    Morph_list = []
+
+    pprint.pprint(All_Morph_list[2])
+    for i in range(len(All_Morph_list[2])):
+        x = All_Morph_list[2][i]
+        print(x.surface, x.base, x.pos, x.pos1)
+
+if __name__ == '__main__':
+    main()
