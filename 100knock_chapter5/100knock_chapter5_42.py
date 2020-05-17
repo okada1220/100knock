16b2cc9b6f1eff40f271230d508262a49d3c24c2
BC029 <1220.naoki7@gmail.com>
2020-05-17 21:29:58 +0900

    進捗
    6~11行目：get_chunk_surface(chunk):はChunk（文節）の文章上におけるsurfaceを返す関数です。
    15~22行目：get_chunk_surfaceによって各Chunkごとのテキストを取得します。dstにより係り受けの関係を持つChunkをタブを間に挟んで表示します。19行目は根ノードにあたるものだけ自分自身に係るようにしたので、それを除去するためのif文です。

---
+++ 100knock_chapter5/100knock_chapter5_42.py
@@ -0,0 +1,25 @@
+import MeCab
+import re
+import pprint
+chapter5_41 = __import__('100knock_chapter5_41')
+
+def get_chunk_surface(chunk):
+    chunk_surface = ''
+    for morph in chunk.morphs:
+        if not morph.surface == '。' and not morph.surface == '、':
+            chunk_surface += morph.surface
+    return chunk_surface
+
+def main():
+    All_Chunk_list = chapter5_41.get_All_Chunk_list('neko.txt.cabocha')
+    for j in range(len(All_Chunk_list)):
+        for i in range(len(All_Chunk_list[j])):
+            chunk = All_Chunk_list[j][i]
+            chunk_surface = get_chunk_surface(chunk)
+            if not (chunk.dst - 1) == i:
+                target_chunk = All_Chunk_list[j][chunk.dst - 1]
+                target_chunk_surface = get_chunk_surface(target_chunk)
+                print(chunk_surface + '\t' + target_chunk_surface)
+
+if __name__ == '__main__':
+    main()
