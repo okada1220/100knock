c57e3a228e6e8de1d41aa128aad8559d772c2a57
BC029 <1220.naoki7@gmail.com>
2020-05-17 21:39:12 +0900

    進捗
    7~12行目：check_chunk(chunk, pos):はChunk内に指定したposを持つMorphが存在するか返します。含まれる場合は１、含まれない場合は０です。後に使うN_flagをif文に直接使わなかったので0,1のflagにしました（bool型にした方がよかったでしょうか？）
    21,25,26行目：check_chunkを使って名詞、動詞が含まれるかをN_flag,V_flagで示し、含まれる場合のみ表示するようにしました。表示することが決まってからChunkのsurfaceを求めた方がいいと思ったので27行目にget_chunk_surfaceを書きました。

---
+++ 100knock_chapter5/100knock_chapter5_43.py
@@ -0,0 +1,31 @@
+import MeCab
+import re
+import pprint
+chapter5_41 = __import__('100knock_chapter5_41')
+chapter5_42 = __import__('100knock_chapter5_42')
+
+def check_chunk(chunk, pos):
+    pos_flag = 0
+    for morph in chunk.morphs:
+        if morph.pos == pos:
+            pos_flag = 1
+    return pos_flag
+
+def main():
+    All_Chunk_list = chapter5_41.get_All_Chunk_list('neko.txt.cabocha')
+    for j in range(len(All_Chunk_list)):
+        for i in range(len(All_Chunk_list[j])):
+            N_flag = 0
+            V_flag = 0
+            chunk = All_Chunk_list[j][i]
+            N_flag = check_chunk(chunk, '名詞')
+            chunk_surface = chapter5_42.get_chunk_surface(chunk)
+            if not (chunk.dst - 1) == i:
+                target_chunk = All_Chunk_list[j][chunk.dst - 1]
+                V_flag = check_chunk(target_chunk, '動詞')
+                if N_flag == 1 and V_flag == 1:
+                    target_chunk_surface = chapter5_42.get_chunk_surface(target_chunk)
+                    print(chunk_surface + '\t' + target_chunk_surface)
+
+if __name__ == '__main__':
+    main()
