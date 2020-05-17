019c82aab30d8a9efb7b7229cda71d04388a7075
BC029 <1220.naoki7@gmail.com>
2020-05-17 16:56:37 +0900

    進捗
    6~12行目：class Chunkを定義しました。set_srcsは係り元を後でまとめて設定するためです。
    15~18行目：mecabはMeCabを行うためです。All_Chunk_listは一文ごとにChunk_listをまとめるためです。fragは最初だけ定義のための初期化を行うためです。dst_compileは係り受けの関係を調べるために---Dのような部分を抽出するためです。
    21~24行目：Chunk_listとitemは一文ごとで使い切りにしたいので最初だけ定義して、文の終わりのEOSで0になるようにしています。
    29~31行目：行中から係り受けの関係---Dなどを取って係り先の項番号をdstに代入しています。
    38行目：chapter5_40.と同様にMorphのリストを作成して係り先のdstと一緒にChunkクラスにして、一行ごとにChunk_listに加えています。（+=なのは一文で一つのリストにするためです。）
    39~43行目：文の終わりを示すEOSが出て、Chunk_listに何か入っていたらAll_Chunk_listに加えます。
    45~48行目：Chunkに入れたdstから係り元の項（target）のsrcsに係り先の項番号（y+1）を加えてます。
    50~57行目：８文目の文節の文字列（形態素で分割されてはいますが）、係り先の項番号、係り元の項番号を表示しています。
    
    足りない部分に処理と判断を加えていってるせいで魔改造になってしまってすみません。

---
+++ 100knock_chapter5/100knock_chapter5_41.py
@@ -0,0 +1,60 @@
+import MeCab
+import re
+import pprint
+chapter5_40 = __import__('100knock_chapter5_40')
+
+class Chunk:
+    def __init__(self, morphs, dst):
+        self.morphs = morphs
+        self.dst = dst
+        self.srcs = []
+    def set_srcs(self, srcs):
+        self.srcs.append(srcs)
+
+def main():
+    mecab = MeCab.Tagger()
+    All_Chunk_list = []
+    frag = 0
+    dst_compile = re.compile('[-+D]')
+    with open('neko.txt.cabocha', 'r', encoding='utf-8') as file:
+        for line in file:
+            if frag == 0:
+                Chunk_list = []
+                item = 0
+                frag = 1
+            if not line == 'EOS\n':
+                Morph_list = []
+                item +=1
+                line.strip()
+                dst_search = dst_compile.search(line)
+                dst = int((line.count('-') + 1)/ 2) + item
+                line = re.sub(r'[-+D]|\s|\|', '', line)
+                word = mecab.parse(line).split('\n')
+                word.remove('')
+                word.remove('EOS')
+                for morph_info in word:
+                    morph_list = re.split(r'\t|,', morph_info)
+                    Morph_list += [chapter5_40.Morph(morph_list)]
+                Chunk_list += [Chunk(Morph_list, dst)]
+            else:
+                if len(Chunk_list) > 0:
+                    All_Chunk_list.append(Chunk_list)
+                    Chunk_list = []
+                    item = 0
+
+    for x in range(len(All_Chunk_list)):
+        for y in range(len(All_Chunk_list[x])):
+            target = All_Chunk_list[x][y]
+            All_Chunk_list[x][target.dst - 1].set_srcs(y + 1)
+
+    pprint.pprint(All_Chunk_list[7])
+    for i in range(len(All_Chunk_list[7])):
+        z = All_Chunk_list[7][i]
+        for morph in z.morphs:
+            pprint.pprint(morph.surface)
+        print('→ 係り先は' + str(z.dst) + '項目です。')
+        for src in z.srcs:
+            print('← 係り元は' + str(src) + '項目です。')
+
+if __name__ == '__main__':
+    main()
