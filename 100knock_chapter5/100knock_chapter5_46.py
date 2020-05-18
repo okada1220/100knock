628bd7bfe0abcccb8abd2fbdea20fe74505bceba
BC029 <1220.naoki7@gmail.com>
2020-05-18 18:56:25 +0900

    進捗
    8~13行目：celect_morph(chunk, pos):はchunkの中からposであるmorphを取り出して、そのbaseをいれたlistを返します。
    15~27行目：search_connection(chunk_list, chunk, pos):はpos、そのposのmorphを含むchunk、そのchunkを含むchunk_listを引数として指定してます。
    celect_morphでbase_listにposであるmorphのbaseのリストを入れます。
    P_base_chunkはchunkに係るP_chunkの助詞とP_chunkを要素に持つリストです。
    P_base_chunk_listはP_base_chunkを要素にもつ二次元リストです。
    最後にbase_listとP_base_chunk_listを返します。
    29~35行目：separate_base_chunk_list(P_base_chunk_list):は二次元リストであるP_base_chunk_listを転置してからP_base_listとP_chunk_listの二つのリストに分割して返します。
    43.44行目:動詞を含むchunkからchunkに含まれる動詞のbaseのリストV_base_listとそのchunkに係る助詞を持つchunkをその助詞とそのchunkを含むVP_base_chuk_listを得ます。
    46行目：VP_base_chunk_listを助詞に合わせてソートします。
    48~53行目：助詞とその助詞を含むchunkのsurfaceとでVP_base_chunk_listを分割してjoin()しました。最後に最左の動詞の原形、助詞、助詞を含むchunkのsurfaceの順で表示します。

---
+++ 100knock_chapter5/100knock_chapter5_46.py
@@ -0,0 +1,56 @@
+import MeCab
+import re
+import pprint
+chapter5_41 = __import__('100knock_chapter5_41')
+chapter5_42 = __import__('100knock_chapter5_42')
+chapter5_43 = __import__('100knock_chapter5_43')
+
+def celect_morph(chunk, pos):
+    base_list = []
+    for morph in chunk.morphs:
+        if morph.pos == pos:
+            base_list.append(morph.base)
+    return base_list
+
+def search_connection(chunk_list, chunk, pos):
+    base_list = celect_morph(chunk, pos)
+    P_base_chunk_list = []
+    for i in chunk.srcs:
+        if not chunk.dst == i:
+            P_chunk = chunk_list[i-1]
+            P_base_chunk = []
+            for P_morph in P_chunk.morphs:
+                if P_morph.pos == '助詞':
+                    P_base_chunk = [P_morph.base, P_chunk]
+            if len(P_base_chunk) > 0:
+                P_base_chunk_list.append(P_base_chunk)
+    return base_list, P_base_chunk_list
+
+def separate_base_chunk_list(P_base_chunk_list):
+    P_base_list = []
+    P_chunk_list = []
+    for P_base_chunk in P_base_chunk_list:
+        P_base_list.append(P_base_chunk[0])
+        P_chunk_list.append(P_base_chunk[1])
+    return P_base_list, P_chunk_list
+
+def main():
+    All_Chunk_list = chapter5_41.get_All_Chunk_list('neko.txt.cabocha')
+    for j in range(len(All_Chunk_list)):
+        for i in range(len(All_Chunk_list[j])):
+            chunk = All_Chunk_list[j][i]
+
+            if chapter5_43.check_chunk(chunk, '動詞'):
+                V_base_list, VP_base_chunk_list = search_connection(All_Chunk_list[j], chunk, '動詞')
+
+                VP_base_chunk_list.sort(key=lambda x: x[0])
+
+                VP_base_list, VP_chunk_list = separate_base_chunk_list(VP_base_chunk_list)
+                VP_chunk_surface_list = [chapter5_42.get_chunk_surface(VP_chunk) for VP_chunk in VP_chunk_list]
+                VP_base = ' '.join(VP_base_list)
+                VP_chunk_surface = ' '.join(VP_chunk_surface_list)
+
+                print(V_base_list[0] + '\t' + VP_base + '\t' + VP_chunk_surface)
+
+if __name__ == '__main__':
+    main()
