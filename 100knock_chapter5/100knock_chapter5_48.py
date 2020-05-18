689c754a73eb949954eb81426e2c8b5b4926f319
BC029 <1220.naoki7@gmail.com>
2020-05-18 19:14:57 +0900

    進捗
    14行目：名詞が含まれるchunkからパスを始めます。
    17~19行目：根に辿り着くまでdstから係り受けを辿って、リストchunk_surfaceに -> をふくめて加えます。
    22~25行目：chunk_surfaceをjoinで文字列にまとめます。joinでエラーが発生しないようにif文を加えました。

---
+++ 100knock_chapter5/100knock_chapter5_48.py
@@ -0,0 +1,28 @@
+import MeCab
+import re
+import pprint
+chapter5_41 = __import__('100knock_chapter5_41')
+chapter5_42 = __import__('100knock_chapter5_42')
+chapter5_43 = __import__('100knock_chapter5_43')
+
+def main():
+    All_Chunk_list = chapter5_41.get_All_Chunk_list('neko.txt.cabocha')
+    for j in range(len(All_Chunk_list)):
+        for i in range(len(All_Chunk_list[j])):
+            chunk_surface = []
+            chunk = All_Chunk_list[j][i]
+            if chapter5_43.check_chunk(chunk, '名詞'):
+                chunk_surface.append(chapter5_42.get_chunk_surface(chunk))
+
+                while(not chunk.dst in chunk.srcs):
+                    chunk_surface.append(' -> ')
+                    chunk_surface.append(chapter5_42.get_chunk_surface(All_Chunk_list[j][chunk.dst - 1]))
+                    chunk = All_Chunk_list[j][chunk.dst - 1]
+
+                if len(chunk_surface) > 0:
+                    path = ''.join(chunk_surface)
+
+                print(path)
+
+if __name__ == '__main__':
+    main()
