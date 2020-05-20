c1a6929301a90b01f95dff10f863f763204f2cf3
BC029 <1220.naoki7@gmail.com>
2020-05-20 14:31:30 +0900

    進捗
    与えられた文から有向グラフがiris3.pngとして出力されるように出来ました。
    11~15行目：グラフ化したい文の入力です。
    17~40行目：chapter5_41のChunk_listの生成です。今回は一文だけだったので文書全体を読み込むget_All_chunk_listは使わず、そのプログラムの一部だけを取ってきました。
    42~52行目：chapter5_42と同様に係り元の文節をchunkと係り先の文節をtarget_chunkとして係り受けの関係をtextに入力しました。
    textの内容はDOT言語の形式になるようにしました。
    54~55行目：DOT言語の形式になったtextからgraphを作成して、iris3.pngとして出力しました。
    
    追記：
    tree2、iris2はneko.txt.cabochaを入力として一部のグラフ化を試してみた結果です。

--- 100knock_chapter5/100knock_chapter5_44.py
+++ 100knock_chapter5/100knock_chapter5_44.py
@@ -1,12 +1,58 @@
 import MeCab
+import CaboCha
 import re
 import pprint
 import pydotplus
+chapter5_40 = __import__('100knock_chapter5_40')
+chapter5_41 = __import__('100knock_chapter5_41')
+chapter5_42 = __import__('100knock_chapter5_42')
 
 def main():
-    dot_file_name = 'tree.dot'
-    graph = pydotplus.graph_from_dot_file(dot_file_name)
-    graph.write_png("iris.png")
+    print('文を入力してください。')
+    original_text = input()
+    cabocha = CaboCha.Parser()
+    cabocha_text = cabocha.parseToString(original_text)
+    cabocha_line = cabocha_text.split('\n')
+
+    mecab = MeCab.Tagger()
+    Chunk_list = []
+    item = 0
+    dst_compile = re.compile('\-+D')
+    for line in cabocha_line:
+        if not line == 'EOS':
+            Morph_list = []
+            item += 1
+            line = line.strip()
+            dst_search = dst_compile.search(line)
+            dst = int((line.count('-') + 1) / 2) + item
+            line = re.sub(r'\-+D|\s|\|', '', line)
+            word = mecab.parse(line).split('\n')
+            word.remove('')
+            word.remove('EOS')
+            for morph_info in word:
+                morph_list = re.split(r'\t|,', morph_info)
+                Morph_list += [chapter5_40.Morph(morph_list)]
+            Chunk_list += [chapter5_41.Chunk(Morph_list, dst)]
+
+    count = 0
+    for chunk in Chunk_list:
+        count += 1
+        Chunk_list[chunk.dst - 1].set_srcs(count + 1)
+
+    text = 'digraph {\n'
+    text += 'node [fontname = "MS Gothic"];\n'
+    for chunk in Chunk_list:
+        chunk_surface = chapter5_42.get_chunk_surface(chunk)
+        if chunk_surface == '':
+            continue
+        if not chunk.dst in chunk.srcs:
+            target_chunk = Chunk_list[chunk.dst - 1]
+            target_chunk_surface = chapter5_42.get_chunk_surface(target_chunk)
+            text += "\"" + target_chunk_surface + '\" -> \"' + chunk_surface + '\"\n'
+    text += '}\n'
+
+    graph = pydotplus.graph_from_dot_data(text)
+    graph.write_png('iris3.png')
 
 if __name__ == '__main__':
     main()
