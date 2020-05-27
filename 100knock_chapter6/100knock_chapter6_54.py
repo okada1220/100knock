926b5e15ab89b69ea67e4a252e88f794d73ca49e
BC029 <1220.naoki7@gmail.com>
2020-05-27 14:39:29 +0900

    進捗
    7~13行目：train、testの正解率を計測するため、それぞれの入力ベクトルx_train_vec、x_test_vecを用意します。
    16~20行目：入力ベクトルと実際のカテゴリであるy_train、y_testからtrainの正解率をstudy、testの正解率をresultとして表示しています。

---
+++ 100knock_chapter6/100knock_chapter6_54.py
@@ -0,0 +1,23 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pickle
+chapter6_52 = __import__('100knock_chapter6_52')
+
+def main():
+    # ファイルからカテゴリと単語分割に分けます
+    x_train, y_train = chapter6_52.get_dataset('train.feature.txt')
+    x_test, y_test = chapter6_52.get_dataset('test.feature.txt')
+
+    # ベクトル化します
+    vectorizer = TfidfVectorizer()
+    x_train_vec = vectorizer.fit_transform(x_train)
+    x_test_vec = vectorizer.transform(x_test)
+
+    # 学習したモデルから正解率を計測します
+    load_model = pickle.load(open('model.sav', 'rb'))
+    study = load_model.score(x_train_vec, y_train)
+    print(study)
+    result = load_model.score(x_test_vec, y_test)
+    print(result)
+
+if __name__ == '__main__':
+    main()
