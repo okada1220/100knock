f4a829eed4c509815dab062fc920290f6751436e
BC029 <1220.naoki7@gmail.com>
2020-05-27 14:41:42 +0900

    進捗
    8~14行目：chapter6_54と同様にベクトル化された入力を用意します。
    17~21行目：trainによる予測をy_train_pred、testによる予測をy_test_predとし、confusion_matrix()を用いてそれぞれの混同行列をを表示します。

---
+++ 100knock_chapter6/100knock_chapter6_55.py
@@ -0,0 +1,24 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import confusion_matrix
+import pickle
+chapter6_52 = __import__('100knock_chapter6_52')
+
+def main():
+    # ファイルからカテゴリと単語分割に分けます
+    x_train, y_train = chapter6_52.get_dataset('train.feature.txt')
+    x_test, y_test = chapter6_52.get_dataset('test.feature.txt')
+
+    # ベクトル化します
+    vectorizer = TfidfVectorizer()
+    x_train_vec = vectorizer.fit_transform(x_train)
+    x_test_vec = vectorizer.transform(x_test)
+
+    # 学習したモデルから混合行列を計測します
+    load_model = pickle.load(open('model.sav', 'rb'))
+    y_train_pred = load_model.predict(x_train_vec)
+    y_test_pred = load_model.predict(x_test_vec)
+    print(confusion_matrix(y_train, y_train_pred))
+    print(confusion_matrix(y_test, y_test_pred))
+
+if __name__ == '__main__':
+    main()
