3c739c3caf980e2eec006181a6bdd572c184496e
BC029 <1220.naoki7@gmail.com>
2020-06-01 20:46:12 +0900

    進捗
    バイナリーファイルのモデルを読み込んで、United_Statesと類似度の高い10語を表示しています。

---
+++ 100knock_chapter7/100knock_chapter7_62.py
@@ -0,0 +1,9 @@
+import gensim
+import pprint
+
+def main():
+    model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300\GoogleNews-vectors-negative300.bin', binary=True)
+    pprint.pprint(model.most_similar('United_States', topn=10))
+
+if __name__ == '__main__':
+    main()
