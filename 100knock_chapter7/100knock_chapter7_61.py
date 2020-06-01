b577ad1e6a827f3776848557ce25a57b086b5f19
BC029 <1220.naoki7@gmail.com>
2020-06-01 20:45:21 +0900

    進捗
    バイナリーファイルのモデルを読み込んで、United_StatesとU.S.のコサイン類似度を表示しています。

--- 100knock_chapter7/100knock_chapter7_61.py
+++ 100knock_chapter7/100knock_chapter7_61.py
@@ -1,8 +1,9 @@
 import gensim
+import pprint
 
 def main():
-    model = gensim.models.KeyedVectors.load_word2vec_format('models/model.bin', binary=True)
-    model.similarity('United_States', 'U.S.')
+    model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300\GoogleNews-vectors-negative300.bin', binary=True)
+    pprint.pprint(model.similarity('United_States', 'U.S.'))
 
 if __name__ == '__main__':
     main()
