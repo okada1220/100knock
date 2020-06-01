0e205f5324ac9dabec0a76fc2a05154c3e525b4e
BC029 <1220.naoki7@gmail.com>
2020-06-01 20:44:22 +0900

    進捗
    バイナリーファイルのモデルを読み込んでUnited_Statesのベクトル値を表示しています。

--- 100knock_chapter7/100knock_chapter7_60.py
+++ 100knock_chapter7/100knock_chapter7_60.py
@@ -2,8 +2,7 @@ import gensim
 import pprint
 
 def main():
-    model = gensim.models.KeyedVectors.load_word2vec_format(r'C:\Users\naoki\Downloads\GoogleNews-vectors-negative300\GoogleNews-vectors-negative300.bin', binary=True)
-    model.save(r'C:\Users\naoki\Documents\models\model.bin')
+    model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300\GoogleNews-vectors-negative300.bin', binary=True)
     pprint.pprint(model['United_States'])
 
 if __name__ == '__main__':
