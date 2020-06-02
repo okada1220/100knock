539424af47da4543d9ab537f9aad2cb7a4a11cc4
BC029 <1220.naoki7@gmail.com>
2020-06-02 19:03:08 +0900

    修正しました
    writefileの開いて、lineごとにwritefileに書き込むようにしました。
    念のため、元のファイルが残るよう単語と類似度を加えたファイルをquestions-words.txtとは別にquenstions-words-result.txtとして作成しました。

--- 100knock_chapter7/100knock_chapter7_64.py
+++ 100knock_chapter7/100knock_chapter7_64.py
@@ -3,7 +3,7 @@ import gensim
 def main():
     model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300\GoogleNews-vectors-negative300.bin', binary=True)
 
-    writetext = ''
+    writefile = open('questions-words-result.txt', 'w', encoding='utf-8')
     with open('questions-words.txt', 'r', encoding='utf-8') as file:
         for line in file:
             words = line.split()
@@ -12,13 +12,11 @@ def main():
                 vec2 = words[1]
                 vec3 = words[2]
                 most_word = model.most_similar(positive=[vec2, vec3], negative=[vec1], topn=1)
-                writeline = line.strip()
-                writeline += str(most_word[0][0]) + ' ' + str(most_word[0][1]) + '\n'
-                writetext += writeline
+                writeline = line.strip() + ' ' + str(most_word[0][0]) + ' ' + str(most_word[0][1]) + '\n'
+                writefile.write(writeline)
             else:
-                writetext += line
-    with open('questions-words-result.txt', 'w', encoding='utf-8') as writefile:
-        writefile.write(writetext)
+                writefile.write(line)
+    writefile.close()
 
 if __name__ == '__main__':
     main()
