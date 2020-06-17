bf3993a495f235debcea137a853ba7f4651e5368
BC029 <1220.naoki7@gmail.com>
2020-06-18 08:21:27 +0900

    進捗
    ファイルを小さくしてプログラムを実行してみました

--- 100knokc_chapter9/100knock_chapter9_82.py
+++ 100knokc_chapter9/100knock_chapter9_82.py
@@ -3,6 +3,7 @@ from torch import nn
 chapter9_80 = __import__('100knock_chapter9_80')
 chapter9_81_2 = __import__('100knock_chapter9_81(2)')
 
+# file から one-hot ベクトルにしてモデルへの入力の形に合わせて list に入れました
 def make_dataset(filename, word_id_dict, category_table):
     words_vector_list = []
     category_vector_list = []
@@ -21,6 +22,7 @@ def main():
     word_id_dict = chapter9_80.get_word_id_dict('../100knock_chapter6/train.feature.txt')
     vocab = max(word_id_dict.values()) + 1
 
+    # category を対応する数字にします
     category_table = {'b': 0, 't': 1, 'e': 2, 'm': 3}
 
     # RNN を作って、隠れ状態ベクトルを初期化します
@@ -32,15 +34,19 @@ def main():
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters())
 
+    # 学習データを読み込みます
     words_vector_list, category_vector_list = make_dataset('../100knock_chapter6/train.feature.txt', word_id_dict, category_table)
 
-    EPOCH_NUM = 10
+    # エポック回数３回で学習させます
+    EPOCH_NUM = 3
     for epoch in range(EPOCH_NUM):
         accuracy_sum = 0
         loss_sum = 0
         for i in range(len(words_vector_list)):
+            # 初期化します
             model.reset()
             optimizer.zero_grad()
+            # 各事例で損失を求め、重みを更新します
             words_vector = words_vector_list[i]
             category_vector = category_vector_list[i]
             pred_category_vector = model.forward(words_vector)
@@ -49,13 +55,16 @@ def main():
             optimizer.step()
             loss_sum += loss
             accuracy_sum += int(category_vector_list[i] == torch.argmax(pred_category_vector[-1][-1]))
+        # 正答率と損失の平均を求めます
         accuracy = accuracy_sum / len(words_vector_list)
         print('accuracy(train):', accuracy)
         loss_average = loss_sum / len(words_vector_list)
         print('loss(train):', loss_average)
 
+    # 評価データを読み込みます
     words_vector_list, category_vector_list = make_dataset('../100knock_chapter6/test.feature.txt', word_id_dict, category_table)
 
+    # 初期化します
     accuracy_sum = 0
     loss_sum = 0
     model.reset()
@@ -66,10 +75,23 @@ def main():
         loss = criterion(pred_category_vector[-1], category_vector.type(torch.long))
         loss_sum += loss
         accuracy_sum += int(category_vector_list[i] == torch.argmax(pred_category_vector[-1][-1]))
+    # 正答率と損失の平均を求めます
     accuracy = accuracy_sum / len(words_vector_list)
     print('accuracy(test):', accuracy)
     loss_average = loss_sum / len(words_vector_list)
     print('loss(test):', loss_average)
+
+    # mini-train.feature.txtでの学習結果（1000文）
+    # accuracy(train): 0.529
+    # loss(train): tensor(1.1248, grad_fn=<DivBackward0>)
+    # accuracy(train): 0.852
+    # loss(train): tensor(0.4471, grad_fn=<DivBackward0>)
+    # accuracy(train): 0.961
+    # loss(train): tensor(0.1110, grad_fn=<DivBackward0>)
+
+    # mini-test.feature.txtでのテスト結果（500文）
+    # accuracy(test): 0.542
+    # loss(test): tensor(1.5572, grad_fn=<DivBackward0>)
 
 if __name__ == '__main__':
     main()
