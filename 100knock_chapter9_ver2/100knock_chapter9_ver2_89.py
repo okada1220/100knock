46f6f64774c36857af7070eb6b7404c09a98d0cf
BC029 <1220.naoki7@gmail.com>
2020-10-03 20:12:04 +0900

    進捗

--- 100knock_chapter9_ver2/100knock_chapter9_ver2_89.py
+++ 100knock_chapter9_ver2/100knock_chapter9_ver2_89.py
@@ -8,11 +8,13 @@ import argparse
 import tqdm
 import models
 
+# 前処理を行うクラスです
 class MyPreproccesing(object):
     def __init__(self, tokenizer, category_table):
         self.tokenizer = tokenizer
         self.category_table = category_table
 
+    # ファイルからタイトルとカテゴリをそれぞれＩＤ化します
     def trans_file(self, filepath):
         with open(filepath, 'r', encoding='utf-8') as file:
             categories = []
@@ -25,6 +27,7 @@ class MyPreproccesing(object):
                 titles.append(title_info)
         return categories, titles
 
+# データセットを作るクラスです
 class MyDataset(Dataset):
     def __init__(self, categories, titles):
         self.data_num = len(titles)
@@ -39,6 +42,7 @@ class MyDataset(Dataset):
         out_label = self.label[idx]
         return out_data, out_label
 
+# DataLoader で使うパディング用の関数です
 def pad_collate(batch):
     title_info, category = zip(*batch)
     max_len = max([title_len for title, title_len in title_info])
@@ -55,15 +59,18 @@ def pad_collate(batch):
     return titles, attention_masks, category
 
 def main():
+    # コマンドライン引数から必要な情報を得ます
     parser = argparse.ArgumentParser()
     parser.add_argument('filepath', help='python 100knock_chapter9_ver2_89 [train_filepath]')
     parser.add_argument('--batch', type=int, default=16)
     parser.add_argument('--lr', type=float, default=0.01)
     args = parser.parse_args()
 
+    # 前処理に必要なものを用意します
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
     category_table = {'b': 0, 't': 1, 'e': 2, 'm': 3}
 
+    # 前処理、データセット、データローダを作ります
     mypreprocessing = MyPreproccesing(tokenizer, category_table)
     categories, titles = mypreprocessing.trans_file(args.filepath)
     train_dataset = MyDataset(categories, titles)
@@ -72,6 +79,7 @@ def main():
     loss = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
 
+    # 訓練を行います
     epoch_num = 10
     for epoch in range(epoch_num):
         epoch_loss = []
@@ -94,8 +102,9 @@ if __name__ == '__main__':
     main()
 
 """
-<実行結果>(訓練データにmini-train.txt(1000事例)を利用)
+<実行結果>
 
+(訓練データにmini-train.txt(1000事例)を利用(batch=16, lr=0.01)
 Epoch: 1
 100%|█████████████████████████████████████████████████████| 63/63 [10:26<00:00,  9.94s/it]
 損失： 2.5799834179499794       正解率： 0.38
@@ -126,4 +135,36 @@ Epoch: 9
 Epoch: 10
 100%|█████████████████████████████████████████████████████| 63/63 [13:07<00:00, 12.50s/it]
 損失： 1.802176937224373        正解率： 0.38
+
+(訓練データにtrain.txt(約10000事例)を利用(batch=32, lr=0.01))
+Epoch: 1
+100%|██████████████████████████████████████████████████████████████████████████████| 334/334 [1:35:28<00:00, 17.15s/it]
+損失： 1.563155860244157        正解率： 0.3948895544739798
+Epoch: 2
+100%|██████████████████████████████████████████████████████████████████████████████| 334/334 [1:46:47<00:00, 19.18s/it]
+損失： 1.6133469762559423       正解率： 0.3852489704230625
+Epoch: 3
+100%|██████████████████████████████████████████████████████████████████████████████| 334/334 [2:12:25<00:00, 23.79s/it]
+損失： 1.4641170046643583       正解率： 0.38393859977536504
+Epoch: 4
+100%|██████████████████████████████████████████████████████████████████████████████| 334/334 [1:46:26<00:00, 19.12s/it]
+損失： 1.4876200965421642       正解率： 0.39086484462748033
+Epoch: 5
+100%|██████████████████████████████████████████████████████████████████████████████| 334/334 [1:41:11<00:00, 18.18s/it]
+損失： 1.4842760905534207       正解率： 0.38880569075252713
+Epoch: 6
+100%|██████████████████████████████████████████████████████████████████████████████| 334/334 [1:40:56<00:00, 18.13s/it]
+損失： 1.447159011385398        正解率： 0.39648071883189817
+Epoch: 7
+100%|██████████████████████████████████████████████████████████████████████████████| 334/334 [1:39:47<00:00, 17.93s/it]
+損失： 1.5100286338857547       正解率： 0.3813178584799701
+Epoch: 8
+100%|██████████████████████████████████████████████████████████████████████████████| 334/334 [1:41:45<00:00, 18.28s/it]
+損失： 1.510240693292218        正解率： 0.38871209284912017
+Epoch: 9
+100%|██████████████████████████████████████████████████████████████████████████████| 334/334 [1:43:23<00:00, 18.57s/it]
+損失： 1.5646467546146072       正解率： 0.3941407712467241
+Epoch: 10
+100%|██████████████████████████████████████████████████████████████████████████████| 334/334 [1:46:00<00:00, 19.04s/it]
+損失： 1.5034492652930185       正解率： 0.38665293897416697
 """
